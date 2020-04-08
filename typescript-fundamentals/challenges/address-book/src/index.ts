01a3e1deedb9671a4d93161c9af7c244f831e64c
Justin <j6shieh@gmail.com>
2020-04-08 11:23:41 -0700

    convert ts practice

--- typescript-fundamentals/challenges/address-book/src/index.ts
+++ typescript-fundamentals/challenges/address-book/src/index.ts
@@ -1,11 +1,11 @@
 export class AddressBook {
-  contacts = [];
+  contacts: Person[] = [];
 
-  addContact(contact) {
+  addContact(contact: Person) {
     this.contacts.push(contact);
   }
 
-  findContactByName(filter) {
+  findContactByName(filter: Person) {
     return this.contacts.filter(c => {
       if (
         typeof filter.firstName !== "undefined" &&
@@ -24,7 +24,7 @@ export class AddressBook {
   }
 }
 
-export function formatDate(date) {
+export function formatDate(date: Date) {
   return (
     date
       .toISOString()
@@ -33,13 +33,34 @@ export function formatDate(date) {
   );
 }
 
-function getFullName(contact) {
+function getFullName(contact: Person) {
   return [contact.firstName, contact.middleName, contact.lastName]
     .filter(Boolean)
     .join(" ");
 }
 
-export function getVcardText(contact, date = new Date()) {
+interface Person {
+  firstName?: string, 
+  middleName?: string, 
+  lastName?: string,
+  email?: string,
+  salutation?: string,
+  phones?: {
+    [k: string]: string;
+  },
+  addresses?: {
+    [k: string]: {
+      houseNumber: number,
+      city: string,
+      street: string,
+      country: string,
+      state: string,
+      postalCode: number
+    };
+  }
+}
+
+export function getVcardText(contact: Person, date = new Date()) {
   const parts = [
     "BEGIN:VCARD",
     "VERSION:2.1",
