8fda93eb24b3f705673e64a43f19980a6d1eccfc
Justin <j6shieh@gmail.com>
2020-04-09 10:14:46 -0700

    top types

--- typescript-fundamentals/challenges/dict/src/index.ts
+++ typescript-fundamentals/challenges/dict/src/index.ts
@@ -1,7 +1,25 @@
-export type Dict = {};
+// <T> captures the generic type of the variable/function 
+// allowing this type to be used elsewhere to make the
+// function more generic
+
+export type Dict<T> = {
+  [k: string]: T | undefined; 
+};
 
 // Array.prototype.map, but for Dict
-export function mapDict() {}
+export function mapDict<T, S>(
+  dict: Dict<T>,
+  fn: (arg: T, idx: number) => S
+): Dict<S> {
+  const out: Dict<S> = {};
+  Object.keys(dict).forEach((dKey, idx) => {
+    const item = dict[dKey];
+    if(typeof item !== 'undefined') {
+      out[dKey] = fn(item,idx);
+    }
+  });
+  return out;
+}
 
 // Array.prototype.reduce, but for Dict
-export function reduceDict() {}
+// export function reduceDict<T>(dict: Dict<T>) {}
